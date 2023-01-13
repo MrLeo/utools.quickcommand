b11758b7c81ea1cea3fbcb0d4588a4b2c6f07fa3
Leo <leo@xuebin.me>
2023-01-13 10:48:03 +0800

    神策上报数据解密

---
+++ src/sa.js
@@ -0,0 +1,7 @@
+let input = quickcommand.readClipboard()
+
+input = decodeURIComponent(input)
+
+input = Buffer.from(input, "base64").toString()
+
+console.log(JSON.stringify(JSON.parse(input), null, 2))
